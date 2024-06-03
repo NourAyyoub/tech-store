f14adf8e1e37c626a729165fa3721acec4be1824
Nour Ayyoub <nour.ayyoub360@gmail.com>
2024-06-03 20:03:30 +0300

    add API to bestSellers

--- src/components/home/BestSellers/BestSellers.jsx
+++ src/components/home/BestSellers/BestSellers.jsx
@@ -1,53 +1,44 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Heading from "../Products/Heading";
 import Product from "../Products/Product";
-import {
-  bestSellerOne,
-  bestSellerTwo,
-  bestSellerThree,
-  bestSellerFour,
-} from "../../../assets/images/index";
+import ErrorMessage from "../../Message/ErrorMessage";
 
 export default function BestSellers() {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/product")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch(() => {
+        setError("There was an error fetching the data!");
+      });
+  }, []);
+
   return (
     <div className="w-full pb-20">
       <Heading heading="Our Bestsellers" />
       <div className="w-full grid grid-cols-1 md:grid-cols-2 lgl:grid-cols-3 xl:grid-cols-4 gap-10">
-        <Product
-          _id="1011"
-          img={bestSellerOne}
-          productName="Flower Base"
-          price="35.00"
-          color="Blank and White"
-          badge={true}
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
-        />
-        <Product
-          _id="1012"
-          img={bestSellerTwo}
-          productName="New Backpack"
-          price="180.00"
-          color="Gray"
-          badge={false}
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
-        />
-        <Product
-          _id="1013"
-          img={bestSellerThree}
-          productName="Household materials"
-          price="25.00"
-          color="Mixed"
-          badge={true}
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
-        />
-        <Product
-          _id="1014"
-          img={bestSellerFour}
-          productName="Travel Bag"
-          price="220.00"
-          color="Black"
-          badge={false}
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
-        />
+        {error ? (
+          <ErrorMessage message={error} />
+        ) : (
+          products.map((product) => (
+            <Product
+              key={product.id}
+              _id={product.id.toString()}
+              img={product.image_url}
+              productName={product.name}
+              price={product.price.toFixed(2)}
+              color={product.category}
+              badge={product.number_of_times_requested > 100}
+              des={product.description}
+            />
+          ))
+        )}
       </div>
     </div>
   );
