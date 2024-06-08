edcd90bf18fdcb71dff8609c3a50891ce380b42c
AmeedHudhud <ameedhudhud0@gmail.com>
2024-06-08 21:31:53 +0300

    modification error in new arrivals

--- src/components/home/NewArrivals/NewArrivals.jsx
+++ src/components/home/NewArrivals/NewArrivals.jsx
@@ -1,76 +1,45 @@
-import React, { useState, useEffect } from "react";
-import Slider from "react-slick";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Heading from "../Products/Heading";
 import Product from "../Products/Product";
-import SampleNextArrow from "./SampleNextArrow";
-import SamplePrevArrow from "./SamplePrevArrow";
+import ErrorMessage from "../../Message/ErrorMessage";
 
 export default function NewArrivals() {
   const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios.get("http://127.0.0.1:8000/api/latestproducts")
-      .then(response => {
+    axios
+      .get("http://localhost:8000/api/latestproducts")
+      .then((response) => {
         setProducts(response.data);
       })
-      .catch(error => {
-        console.error("There was an error fetching the products!", error);
+      .catch(() => {
+        setError("There was an error fetching the data!");
       });
   }, []);
-
-  const settings = {
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
-    responsive: [
-      {
-        breakpoint: 1025,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-        },
-      },
-      {
-        breakpoint: 769,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-        },
-      },
-    ],
-  };
 
   return (
     <div className="w-full pb-16">
       <Heading heading="New Arrivals" />
-      <Slider {...settings}>
-        {products.map(product => (
-          <div key={product.id} className="px-2">
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 lgl:grid-cols-3 xl:grid-cols-4 gap-10">
+        {error ? (
+          <ErrorMessage message={error} />
+        ) : (
+          products.map((product) => (
             <Product
-              _id={product.id}
+              key={product.id}
+              _id={product.id.toString()}
               img={product.image_url}
               productName={product.name}
-              price={product.price}
+              price={product.price.toFixed(2)}
+              color={product.category}
               badge={product.number_of_times_requested > 0}
               des={product.description}
             />
-          </div>
-        ))}
-      </Slider>
+          ))
+        )}
+      </div>
     </div>
   );
 }
